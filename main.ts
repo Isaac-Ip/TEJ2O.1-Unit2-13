7b32ee9b4f24d4c0bde65f8edb8f84a2a45a64b8
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-10-15 23:42:09 -0400

    while statements

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Ip
+ * Created on: Oct 2025
+ * This program uses while statements.
 */
 
-basic.showString('Hello, World!')
+// variables
+let loopCounter = 4
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    while (loopCounter > 0) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+        basic.pause(1000)
+        loopCounter -= 1
+    }
+})
